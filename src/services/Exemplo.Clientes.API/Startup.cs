82b78e8a4131f67eecc75f72ef3a71c55b704fba
Thiago Nishio <thiago.nishio@soulisto.com.br>
2020-05-28 12:19:31 -0300

    Limpando using desnecessários

--- src/services/Exemplo.Clientes.API/Startup.cs
+++ src/services/Exemplo.Clientes.API/Startup.cs
@@ -1,20 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Exemplo.Clientes.API.Application.Commands;
-using Exemplo.Clientes.API.Application.Events;
 using Exemplo.Core.Mediator;
-using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Exemplo.Clientes.API
 {
