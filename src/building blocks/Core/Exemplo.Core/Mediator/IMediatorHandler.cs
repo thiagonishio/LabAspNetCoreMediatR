b44dc13ace0a7d8be70c1578bc03719137e4cbac
Thiago Nishio <thiago@aspbrasil.net>
2020-05-27 23:32:56 -0300

    Add project files.

---
+++ src/building blocks/Core/Exemplo.Core/Mediator/IMediatorHandler.cs
@@ -0,0 +1,12 @@
+﻿using Exemplo.Core.Messages;
+using FluentValidation.Results;
+using System.Threading.Tasks;
+
+namespace Exemplo.Core.Mediator
+{
+    public interface IMediatorHandler
+    {
+        Task PublicarEvento<T>(T evento) where T : Event;
+        Task<ValidationResult> EnviarComando<T>(T comando) where T : Command;
+    }
+}
